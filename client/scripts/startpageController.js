fe6435d8ca3137b07ab237fc9f8460d241d24458
Jessica Andersson <ja@speedledger.se>
2015-12-05 16:12:07 +0100

    Added eventhandler for buttons

---
+++ client/scripts/startpageController.js
@@ -0,0 +1,10 @@
+Template.startpage.events({
+    "click #contributionRedirect": function() {
+        console.log('Yo');
+        //Router.go('/home');
+    },
+    "click #organizationRedirect": function() {
+        console.log('Yloo');
+        //Router.go('/home');
+    }
+});
