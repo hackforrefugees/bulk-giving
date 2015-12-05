1666a159e9da1df8c2c774d2cba957a7c6f61f13
Sofia Larsson <karinsofiapaulina@gmail.com>
2015-12-05 16:57:23 +0100

    Only possible to add need if user belongs to organization

--- client/scripts/organizationController.js
+++ client/scripts/organizationController.js
@@ -2,12 +2,11 @@ Template.organization.events({
     "submit .new-need": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
- 
+
       // Get value from form element
       var need = event.target.what.value;
       var amount = event.target.amount.value;
-      console.log("adding need with id : " + Meteor.call("addNeed", need, amount, this.name, this._id));
-      console.log("New need has been added : " + need + "  in amount of " + amount);
+      Meteor.call("addNeed", need, amount, this.name, this._id);
     }
 
 });
--- server/organizations.js
+++ server/organizations.js
@@ -12,18 +12,19 @@ Meteor.methods({
     createOrganization: function(organizationName) {
         var id = Meteor.userId();
         if(id != null) {
-            //returns the _id of the newly created record        
-            var newId = Organizations.insert({name: organizationName, owner:id});
-            return newId;
-        }        
+            //returns the _id of the newly created record
+            Meteor.users.update({_id: id}, {$set:{organization: organizationName}})
+            var organizationId = Organizations.insert({name: organizationName, owner:id});
+            return organizationId;
+        }
     },
 
    addNeed: function(title, amount, organizationName, organizationId) {
-        var userId = Meteor.userId();
-        // if(userId != null) {
-            var needId = Needs.insert({title: title, needed: amount, planned: 0, delivered: 0, organization: organizationName, creator: userId });
+        var user = Meteor.users.findOne(this.userId);
+        if(user.organization == organizationName) {
+            var needId = Needs.insert({title: title, needed: amount, planned: 0, delivered: 0, organization: organizationName, creator: this.userId });
             return needId;
-        // }        
+        }
     }
 });
 
