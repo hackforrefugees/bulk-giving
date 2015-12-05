aacac61ec767ca31af0e03310a2dec81bab1811c
Anton Shumeika <shumeikaanton@gmail.com>
2015-12-05 20:22:08 +0100

    make sure only existing organizations are shown, ortherwise redirect to org list

--- client/router.js
+++ client/router.js
@@ -13,9 +13,17 @@ Router.route('/',{
 Router.route('/home');
 Router.route('/organizations');
 Router.route('/organizations/:name', function() {
+	var name  =this.params.name;
+	// making sure the organization exists otherwise redirect to all organizations list
+	if(!Organizations.findOne({name: name})){
+		Router.go('/organizations');
+	}
 	this.render("organization", {data: function(){ 
-		var name = this.params.name; 
+		var name = this.params.name;
 		return Organizations.findOne({name: name}) 
 	}})
+});
+Router.route('/organizations/(.*)', function() {
+	this.render("organizations"); 
 });
 Router.route('/signin');
