534eb9593360f9c0fadb20c81dbcac66753a4e4d
Eng <c@rlengstrom.se>
2015-12-05 12:33:39 +0100

    Added iron router and a basic collection

--- client/client.js
+++ client/client.js
@@ -1,14 +1,22 @@
 Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.body.events({
+    "submit .new-organization": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+ //     Tasks.insert({
+ //       text: text,
+ //       createdAt: new Date() // current time
+//      });
+     console.log("Yay!");
+ 
+      // Clear form
+      event.target.text.value = "";
 
-  Template.hello.events({
-    'click': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
     }
   });
