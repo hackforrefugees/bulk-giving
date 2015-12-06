29542651ead880b94878dd294896e811b7c442bb
Eng <c@rlengstrom.se>
2015-12-06 13:52:26 +0100

    Organizations are now sorted alphabetically

--- client/scripts/organizations.js
+++ client/scripts/organizations.js
@@ -2,6 +2,12 @@ Meteor.subscribe("allOrganizations");
 
 Template.organizationList.helpers({
     organizations: function() {
-      return Organizations.find({});
+      return Organizations.find();
+    },
+
+    organizationsSortedCaseInsensitive: function() {
+    	return Organizations.find().fetch().sort(function (a, b) {
+    		return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+		});
     }
 });
